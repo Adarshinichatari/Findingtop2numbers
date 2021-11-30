aae4179ab3e190dc55d36ebeb56e60f8997f6506
Adarshini Chatari <chatariadarshini73@gmail.com>
2021-11-30 12:13:44 +0530

    Found top 2 numbers

---
+++ src/Finding2topNumbers.java
@@ -0,0 +1,23 @@
+public class Finding2topNumbers {
+    public void printTwoTop2Numbers(int[] numbers){
+            int topOne = 0;
+            int topTwo = 0;
+            for(int n:numbers){
+                if(topOne < n){
+                    topTwo = topOne;
+                    topOne =n;
+                } else if(topTwo < n){
+                    topTwo = n;
+                }
+            }
+            System.out.println("First Highest Number is: "+topOne);
+            System.out.println("Second Highest Number is: "+topTwo);
+        }
+
+        public static void main(String a[]){
+            int numbers[] = {11,22,45,92,21,54,79,11};
+            Finding2topNumbers tmn = new Finding2topNumbers();
+            tmn.printTwoTop2Numbers(numbers);
+        }
+    }
+
